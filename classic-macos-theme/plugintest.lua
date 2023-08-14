e286dd811c12dc3ce19de5e07582e6c0ccb7899f
Nogi <108937010+closebridge@users.noreply.github.com>
2023-08-14 09:36:28 +0700

    theme!

---
+++ classic-macos-theme/plugintest.lua
@@ -0,0 +1,16 @@
+local Plugin = {
+    ["PluginName"] = "Mac OS 9 Theme",
+    ["PluginDescription"] = "Another custom theme for IY, by "uolol & anyanal" (Apple Inc. for Mac OS 9)",
+    ["Commands"] = {
+        ["classicmacos9"] = {
+            ["ListName"] = "classicmacos9",
+            ["Description"] = "Enable Mac OS 9's Theme",
+            ["Aliases"] = {"macos9"},
+            ["Function"] = function(args,speaker)
+                
+            end
+        }
+     }
+}
+
+return Plugin
